933d946c3e21d37f4b5cca420f83a40624307d38
Mukesh <mukesh@biorev.studio>
2025-06-20 19:25:45 +0530

    sonar test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
     <>
       <h1>Github Actions</h1>
       <h2>It is  a  KT  demo  for  cicd pipeline by  </h2>
+      <h3>Mukesh </h3>
+      <h3>Rohit</h3>
+      <h3>Ravi</h3>
+      <h3>Rohit</h3>
+
     </>
   )
 }
